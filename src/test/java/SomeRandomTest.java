31969a37cce20ec3f8c028ad6c540d09a2de8d17
alisitsky <79695539+alisitsky@users.noreply.github.com>
2023-07-21 17:18:54 +0300

    Update SomeRandomTest.java

--- src/test/java/SomeRandomTest.java
+++ src/test/java/SomeRandomTest.java
@@ -1,8 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class SomeRandomTest {
 
     @Test
     public void testExample() {
+
+        open("https://yandex.ru");
+        
     }
 }
